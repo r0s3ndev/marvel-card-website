bf43b90125d30270995f4be87a35cceb6c0549c3
ferry <ferry.c.reyes01@gmail.com>
2025-01-03 17:17:14 +0100

    Homepage set-up with basic css styling

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -6,6 +6,7 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
+import Carousel from 'react-bootstrap/Carousel';
 
 
 function Homepage({logoutUser, userData}) {
@@ -14,6 +15,10 @@ function Homepage({logoutUser, userData}) {
 
   console.log("userData -> ", userData);
 
+  const handleShopClick = () => {
+    console.log("shop clicked");
+  }
+
   const handleLogout = async () => {
     const res = await logoutUser();
     if(res.status === 200){
@@ -21,7 +26,7 @@ function Homepage({logoutUser, userData}) {
       setTimeout(() => {
         navigate('/login');
       }, "3000");
-}
+    }
   }
   return (
     <>
@@ -42,9 +47,54 @@ function Homepage({logoutUser, userData}) {
       </Navbar>
       <div className='homepage-container'>
         <div>
-
           <h1>Welcome {userData.username}</h1>
-          
+
+          <div>
+            {/* favorite card & album - collectable card */}
+            <h4>Your favvorite hero: </h4>
+            <img alt={userData.favoriteHeroCard[0].name} src={userData.favoriteHeroCard[0].thumbnail.path + "." + userData.favoriteHeroCard[0].thumbnail.extension}/>
+            <p>{userData.favoriteHeroCard.description ? userData.favoriteHeroCard.description : "no description available"}</p>
+          </div>
+
+          <div className='carousel-shop-div' onClick={handleShopClick}>
+            {/* shop section */}
+            <Carousel fade>
+              <Carousel.Item className='carousel-item'>
+                <img alt="1st_image" src="http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg" text="SHOP" />
+                <Carousel.Caption>
+                  <h3>Buy booster to get more hero</h3>
+                  <p>
+                    SHOP
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className='carousel-item'>
+                <img alt="2nd_image" src='http://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16.jpg' text="SHOP" />
+                <Carousel.Caption>
+                  <h3>Buy booster to get more hero</h3>
+                  <p>
+                    SHOP
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className='carousel-item'>
+                <img alt="3rd_image" src='http://i.annihil.us/u/prod/marvel/i/mg/9/30/535feab462a64.jpg' text="SHOP" />
+                <Carousel.Caption>
+                <h3>Buy booster to get more hero</h3>
+                  <p>
+                    SHOP
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+
+          </div>
+
+          <div>
+            {/* Trade section */}
+
+          </div>
+
         </div>
     
       </div>
